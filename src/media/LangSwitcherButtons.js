fc28dca3b7841ae1add072001d2c0dddbc7a69eb
TRADEBOG <juan.miguel.jaramillo1984@gmail.com>
2023-03-10 15:27:36 -0500

    Botones de Switcher de idioma con animaciones

--- src/media/LangSwitcherButtons.js
+++ src/media/LangSwitcherButtons.js
@@ -3,12 +3,25 @@ import { motion } from "framer-motion";
 
 function LangSwitcherButtons() {
   return (
-    <main className="contenedor-language-switcher">
+    <motion.main
+      className="contenedor-language-switcher"
+      initial={{
+        x: -200,
+      }}
+      animate={{
+        x: 0,
+      }}
+      transition={{
+        type: "tween",
+        duration: 0.5,
+        delay: 0,
+      }}
+    >
       <motion.div
         onClick={() => console.log("Hola")}
         className="transiciones my-2 language-switcher-english"
         initial={{
-          x: -110,
+          x: -112,
         }}
         whileHover={{
           x: -30,
@@ -29,7 +42,7 @@ function LangSwitcherButtons() {
         onClick={() => console.log("Hola")}
         className="transiciones language-switcher-spanish"
         initial={{
-          x: -110,
+          x: -112,
         }}
         whileHover={{
           x: -30,
@@ -39,14 +52,14 @@ function LangSwitcherButtons() {
           duration: 0.3,
         }}
       >
-        <p>SPANISH</p>
+        <p>ESPAÑOL</p>
         <img
-          className="rounded-full w-5 h-5 ml-2 mt-0.5"
+          className="rounded-full w-5 h-5 ml-1 mt-0.5"
           alt="Bandera de España en el cambiador de Idiomas."
           src={require("../assets/spain.png")}
         />
       </motion.div>
-    </main>
+    </motion.main>
   );
 }
 export default LangSwitcherButtons;
