1c5a2b08fde3d2b4b671e9223c199660ef4de20d
TRADEBOG <juan.miguel.jaramillo1984@gmail.com>
2023-03-13 02:26:59 -0500

    Codigo limpiado en LangSplash.js

--- src/media/LangSplash.js
+++ src/media/LangSplash.js
@@ -52,7 +52,7 @@ function LangSplash() {
         </div>
         <div className="contenedor-botones-idioma">
           <IconButton
-            className="boton-conoce-mas boton-idioma transiciones text-sm 2xl:text-2xl xl:text-xl lg:text-xl md:text-lg sm:text-sm hover:scale-105"
+            className="boton-conoce-mas boton-idioma transiciones estilos-interactivos-botones"
             leftIcon={
               <img
                 className="icono-language-switcher mb-1.5 ml-0"
@@ -70,7 +70,7 @@ function LangSplash() {
             }}
           />
           <IconButton
-            className="boton-conoce-mas boton-idioma transiciones text-sm 2xl:text-2xl xl:text-xl lg:text-xl md:text-lg sm:text-sm hover:scale-105"
+            className="boton-conoce-mas boton-idioma transiciones estilos-interactivos-botones"
             leftIcon={
               <img
                 className="icono-language-switcher mb-1.5 ml-0"
