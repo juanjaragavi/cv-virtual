ae50e51b74cf4de9f79ce6ba2485fb963fe40021
TRADEBOG <juan.miguel.jaramillo1984@gmail.com>
2023-03-14 19:12:42 -0500

    Lista la interna Dummy. Sigue la animación de ints

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,71 @@
-import React from 'react'
+import React from "react";
+import ContColumnas from "../layouts/ContColumnas";
+import Columna from "../layouts/Columna";
+import { useTranslation } from "react-i18next";
 
-export default function Footer() {
+function Footer() {
+  const { t } = useTranslation("common");
+  const EmailJJ = () => {
+    window.open("mailto:info@juanjaramillo.tech", "_parent");
+  };
+  const estiloPrimeraColumna = {
+    border: 0,
+    borderColor: "white",
+    borderRadius: 0,
+    flexBasis: "50%;",
+    margin: 0,
+  };
+  const estilosColumnas = {
+    borderLeftWidth: 1,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRightWidth: 0,
+    borderColor: "white",
+    borderRadius: 0,
+    flexBasis: "33.3333333%;",
+    margin: 0,
+  };
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer-general">
+      <ContColumnas>
+        <Columna style={estiloPrimeraColumna}>
+          <h4 className="font-medium text-xl">
+            © 2023{" "}
+            <span className="titulo-degradado font-bold">
+              {t("Nombre.title", { framework: "React" })}
+            </span>
+          </h4>
+          <p className="font-light text-[1rem] -mt-2.5">
+            {t("DerechosReservados.title", { framework: "React" })}
+          </p>
+        </Columna>
+        <Columna style={estilosColumnas}>
+          <a
+            href={EmailJJ}
+            onClick={EmailJJ}
+            className="group hover:cursor-pointer text-xl"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-mail stroke-pink-500 inline pb-0.5 mr-1 group-hover:scale-110 transiciones"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path>
+              <path d="M3 7l9 6l9 -6"></path>
+            </svg>
+            info@juanjaramillo.tech
+          </a>
+        </Columna>
+      </ContColumnas>
+    </footer>
+  );
 }
+export default Footer;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,26 +1,38 @@
 import React from "react";
-import FotoSinBorde from "../media/FotoSinBorde";
+import Columna from "../layouts/Columna";
+import Seccion from "../layouts/Seccion";
+import Footer from "../components/Footer";
 import FotoBorde from "../media/FotoBorde";
 import { useTranslation } from "react-i18next";
+import FotoSinBorde from "../media/FotoSinBorde";
 import TextoEjemplo from "../media/TextoEjemplo";
 import ContInternas from "../layouts/ContInternas";
-import Columna from "../layouts/Columna";
 import ContColumnas from "../layouts/ContColumnas";
-import Seccion from "../layouts/Seccion";
 
 function About() {
-  const sinBorde = { border: 0 }
-  const sinBordeMarginTop = { border: 0, marginTop: 30 }
-  const paddingInterno = { padding: "1em" };
-  const tresColumnas = { flexBasis: "33.333333%", border: 0 }
+  const sinBorde = { border: 0 };
+  const sinBordeMarginTop = { border: 0, marginTop: 30 };
+  const tresColumnas = { flexBasis: "33.333333%", border: 0 };
+  const tresColumnasBorde = { flexBasis: "33.333333%" };
   const segundaColumna = { order: 9999, flexBasis: "50%", border: 0 };
   const primeraColumna = { order: -9999, border: 0, flexBasis: "50%" };
-  const textoCentrado = { textAlign: "center", paddingleft: 20, paddingRight: 20, marginTop: 10 }
+  const textoCentrado = {
+    textAlign: "center",
+    paddingleft: 20,
+    paddingRight: 20,
+    marginTop: 10,
+  };
   const textoNegrilla = { fontWeight: 700 };
+  const paddingLados = {
+    paddingLeft: "25%",
+    paddingRight: "25%",
+    marginTop: 50,
+    marginBottom: 50,
+  };
 
   const { t } = useTranslation("common");
   return (
-    <ContInternas style={paddingInterno}>
+    <ContInternas>
       <ContColumnas>
         <Columna style={primeraColumna}>
           <FotoBorde
@@ -31,8 +43,12 @@ function About() {
         </Columna>
         <Columna style={segundaColumna}>
           <h3 className="titulo-3">Desde 2007,</h3>
-          <h1 className="titulo-1 titulo-degradado -mt-2 -mb-3">Obsesionado</h1>
-          <h3 className="titulo-3 decoracion-texto-destacado">con la Tecnología y el Marketing</h3>
+          <h1 className="titulo-1 titulo-degradado -mt-2 -mb-3">
+            Obsesionado
+          </h1>
+          <h3 className="titulo-3 decoracion-titulo-destacado">
+            con la Tecnología y el Marketing
+          </h3>
           <TextoEjemplo style={textoCentrado} />
         </Columna>
       </ContColumnas>
@@ -42,19 +58,43 @@ function About() {
           <h1 className="titulo-1 titulo-degradado">Título 1</h1>
           <h2 className="titulo-2 color-texto-primario">Título 2</h2>
           <h3 className="titulo-3 color-texto-primario">Título 3</h3>
-          <h4 className="titulo-4 color-texto-primario"><span style={textoNegrilla}>Título 4:</span> Este es, quizás, el único título que puede ser largo y de más de un renglón. Esto es, porque este puede ser un tipo de título que simplemente reemplace a un párrafo, pero con un texto un poco más destacado, o para infiltrar títulos entre el texto.</h4>
+          <h4 className="titulo-4 color-texto-primario">
+            <span style={textoNegrilla}>Título 4:</span> Este es, quizás, el
+            único título que puede ser largo y de más de un renglón. Esto es,
+            porque este puede ser un tipo de título que simplemente reemplace a
+            un párrafo, pero con un texto un poco más destacado, o para
+            infiltrar títulos entre el texto.
+          </h4>
+        </Columna>
+      </ContColumnas>
+      <ContColumnas>
+        <Columna style={primeraColumna}>
+          <h3 className="titulo-3">Desde 2007,</h3>
+          <h2 className="titulo-2 titulo-degradado -mt-2 -mb-3">Obsesionado</h2>
+          <h4 className="titulo-4 decoracion-texto-destacado">
+            con la Tecnología y el Marketing
+          </h4>
+          <TextoEjemplo style={textoCentrado} />
+        </Columna>
+        <Columna style={segundaColumna}>
+          <FotoBorde
+            src={require("../assets/ejemplo-eliminar.jpg")}
+            title={t("AltFotoInternas.title", { framework: "React" })}
+            alt={t("AltFotoInternas.title", { framework: "React" })}
+          />
         </Columna>
       </ContColumnas>
       <Seccion style={sinBorde}>
         <TextoEjemplo />
         <FotoSinBorde
-            src={require("../assets/logo-juan-jaramillo.png")}
-            title={t("AltFotoInternas.title", { framework: "React" })}
-            alt={t("AltFotoInternas.title", { framework: "React" })}
-          />
+          style={paddingLados}
+          src={require("../assets/logo-juan-jaramillo.png")}
+          title={t("AltFotoInternas.title", { framework: "React" })}
+          alt={t("AltFotoInternas.title", { framework: "React" })}
+        />
       </Seccion>
       <ContColumnas>
-        <Columna style={tresColumnas}>
+        <Columna style={tresColumnasBorde}>
           <TextoEjemplo />
           <FotoBorde
             src={require("../assets/ejemplo-eliminar.jpg")}
@@ -71,13 +111,14 @@ function About() {
           />
         </Columna>
         <Columna style={tresColumnas}>
-        <FotoBorde
+          <FotoBorde
             src={require("../assets/ejemplo-eliminar.jpg")}
             title={t("AltFotoInternas.title", { framework: "React" })}
             alt={t("AltFotoInternas.title", { framework: "React" })}
           />
         </Columna>
       </ContColumnas>
+      <Footer />
     </ContInternas>
   );
 }
