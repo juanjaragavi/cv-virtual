a19e1ac5d6d9f6e15721d58de74e45e58c00f777
TRADEBOG <juan.miguel.jaramillo1984@gmail.com>
2023-06-29 11:43:25 -0500

    Segundo intento con Zapier 6

--- src/modules/FormularioContacto.js
+++ src/modules/FormularioContacto.js
@@ -7,168 +7,169 @@ import { motion, AnimatePresence } from "framer-motion";
 import ConfirmResetForm from "../media/ConfirmResetForm";
 
 function FormularioContacto() {
-    const { t } = useTranslation("common");
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
-    const [confirmarReset, setConfirmarReset] = useState(false);
-    const handleResetClick = () => {
-        setConfirmarReset(true);
-    };
+  const { t } = useTranslation("common");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [confirmarReset, setConfirmarReset] = useState(false);
+  const handleResetClick = () => {
+    setConfirmarReset(true);
+  };
 
-    const handleConfirmClick = () => {
-        document.querySelector("form").reset();
-        setConfirmarReset(false);
-    };
+  const handleConfirmClick = () => {
+    document.querySelector("form").reset();
+    setConfirmarReset(false);
+  };
 
-    const handleCancelClick = () => {
-        setConfirmarReset(false);
-    };
+  const handleCancelClick = () => {
+    setConfirmarReset(false);
+  };
+  const onFormSubmit = (data) => {
+    axios
+      .post("https://hooks.zapier.com/hooks/catch/15793138/3drnxcr/", data)
+      .then((response) => {
+        console.log("Form data sent successfully");
+      })
+      .catch((error) => {
+        console.error("Error sending form data", error);
+      });
+  };
 
-const onSubmit = async (data) => {
-    try {
-        const response = await axios.post('https://hooks.zapier.com/hooks/catch/15793138/3drnxcr/', data);
-        console.log(response);
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-    return (
-        <div className="contenedor-formulario-contacto">
-        <AnimatePresence>
-            {confirmarReset && (
-            <motion.div
-                exit={{
-                opacity: 0,
-                transition: {
-                    type: "tween",
-                    ease: "easeInOut",
-                    duration: 0.3,
-                    delay: 0.3,
-                },
-                }}
-            >
-                <ConfirmResetForm
-                className="contenedor-conf-reset-formulario-contacto"
-                onClickAccept={handleConfirmClick}
-                onClickCancel={handleCancelClick}
-                />
-            </motion.div>
-            )}
-        </AnimatePresence>
-        <form onReset={handleResetClick} onSubmit={handleSubmit(onSubmit)}>
-            <div className="contenedor-campo-formulario-contacto">
-            <input
-                {...register("nombres", {
-                required: true,
-                })}
-                className="spartan-medium campo-formulario-contacto transiciones peer"
-                type="text"
-                name="nombres"
-                id="nombres"
-                placeholder={t("NombresFormContacto.title", { framework: "React" })}
+  return (
+    <div className="contenedor-formulario-contacto">
+      <AnimatePresence>
+        {confirmarReset && (
+          <motion.div
+            exit={{
+              opacity: 0,
+              transition: {
+                type: "tween",
+                ease: "easeInOut",
+                duration: 0.3,
+                delay: 0.3,
+              },
+            }}
+          >
+            <ConfirmResetForm
+              className="contenedor-conf-reset-formulario-contacto"
+              onClickAccept={handleConfirmClick}
+              onClickCancel={handleCancelClick}
             />
-            {errors.nombres?.type === "required" && (
-                <p className="spartan-medium texto-error-formulario-contacto">
-                {t("CampoReqFormContacto.title", { framework: "React" })}
-                </p>
-            )}
-            </div>
-            <div className="contenedor-campo-formulario-contacto">
-            <input
-                {...register("apellidos", {
-                required: true,
-                })}
-                className="spartan-medium campo-formulario-contacto transiciones peer"
-                type="text"
-                name="apellidos"
-                id="apellidos"
-                placeholder={t("ApellidosFormContacto.title", {
-                framework: "React",
-                })}
-            />
-            {errors.apellidos?.type === "required" && (
-                <p className="spartan-medium texto-error-formulario-contacto">
-                {t("CampoReqFormContacto.title", { framework: "React" })}
-                </p>
-            )}
-            </div>
-            <div className="contenedor-campo-formulario-contacto">
-            <input
-                {...register("email", {
-                required: true,
-                pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
-                })}
-                className="spartan-medium campo-formulario-contacto transiciones peer"
-                type="email"
-                name="email"
-                id="email"
-                placeholder={t("EmailFormContacto.title", { framework: "React" })}
-            />
-            {errors.email?.type === "required" && (
-                <p className="spartan-medium texto-error-formulario-contacto">
-                {t("CampoReqFormContacto.title", { framework: "React" })}
-                </p>
-            )}
-            {errors.email?.type === "pattern" && (
-                <p className="spartan-medium texto-error-formulario-contacto">
-                {t("CampoReqFormContacto.title", { framework: "React" })}
-                </p>
-            )}
-            </div>
-            <div className="contenedor-campo-formulario-contacto">
-            <input
-                {...register("telefono", {
-                maxLength: 10,
-                minLength: 10,
-                required: true,
-                type: number,
-                })}
-                className="spartan-medium campo-formulario-contacto transiciones peer"
-                type="number"
-                name="telefono"
-                id="telefono"
-                placeholder={t("TelefonoFormContacto.title", {
-                framework: "React",
-                })}
-            />
-            {errors.telefono?.type === "required" && (
-                <p className="spartan-medium texto-error-formulario-contacto">
-                {t("CampoReqFormContacto.title", { framework: "React" })}
-                </p>
-            )}
-            {errors.telefono?.type === "minLength" && (
-                <p className="spartan-medium texto-error-formulario-contacto">
-                {t("MinCharFormContacto.title", { framework: "React" })}
-                </p>
-            )}
-            {errors.telefono?.type === "maxLength" && (
-                <p className="spartan-medium texto-error-formulario-contacto">
-                {t("MaxCharFormContacto.title", { framework: "React" })}
-                </p>
-            )}
-            </div>
-            <div className="contenedor-botones-formulario-contacto">
-            <button
-                className="spartan-medium boton-enviar-formulario-contacto boton-idioma transiciones estilos-interactivos-botones borde-redondeado"
-                type="submit"
-            >
-                {t("BtnEnviarFormContacto.title", { framework: "React" })}
-            </button>
-            <button
-                className="spartan-medium boton-restablecer-formulario-contacto boton-idioma transiciones estilos-interactivos-botones borde-redondeado"
-                type="button"
-                onClick={handleResetClick}
-            >
-                {t("BtnResetFormContacto.title", { framework: "React" })}
-            </button>
-            </div>
-        </form>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <form onReset={handleResetClick} onSubmit={handleSubmit(onFormSubmit)}>
+        <div className="contenedor-campo-formulario-contacto">
+          <input
+            {...register("nombres", {
+              required: true,
+            })}
+            className="spartan-medium campo-formulario-contacto transiciones peer"
+            type="text"
+            name="nombres"
+            id="nombres"
+            placeholder={t("NombresFormContacto.title", { framework: "React" })}
+          />
+          {errors.nombres?.type === "required" && (
+            <p className="spartan-medium texto-error-formulario-contacto">
+              {t("CampoReqFormContacto.title", { framework: "React" })}
+            </p>
+          )}
         </div>
-    );
+        <div className="contenedor-campo-formulario-contacto">
+          <input
+            {...register("apellidos", {
+              required: true,
+            })}
+            className="spartan-medium campo-formulario-contacto transiciones peer"
+            type="text"
+            name="apellidos"
+            id="apellidos"
+            placeholder={t("ApellidosFormContacto.title", {
+              framework: "React",
+            })}
+          />
+          {errors.apellidos?.type === "required" && (
+            <p className="spartan-medium texto-error-formulario-contacto">
+              {t("CampoReqFormContacto.title", { framework: "React" })}
+            </p>
+          )}
+        </div>
+        <div className="contenedor-campo-formulario-contacto">
+          <input
+            {...register("email", {
+              required: true,
+              pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
+            })}
+            className="spartan-medium campo-formulario-contacto transiciones peer"
+            type="email"
+            name="email"
+            id="email"
+            placeholder={t("EmailFormContacto.title", { framework: "React" })}
+          />
+          {errors.email?.type === "required" && (
+            <p className="spartan-medium texto-error-formulario-contacto">
+              {t("CampoReqFormContacto.title", { framework: "React" })}
+            </p>
+          )}
+          {errors.email?.type === "pattern" && (
+            <p className="spartan-medium texto-error-formulario-contacto">
+              {t("CampoReqFormContacto.title", { framework: "React" })}
+            </p>
+          )}
+        </div>
+        <div className="contenedor-campo-formulario-contacto">
+          <input
+            {...register("telefono", {
+              maxLength: 10,
+              minLength: 10,
+              required: true,
+              type: number,
+            })}
+            className="spartan-medium campo-formulario-contacto transiciones peer"
+            type="number"
+            name="telefono"
+            id="telefono"
+            placeholder={t("TelefonoFormContacto.title", {
+              framework: "React",
+            })}
+          />
+          {errors.telefono?.type === "required" && (
+            <p className="spartan-medium texto-error-formulario-contacto">
+              {t("CampoReqFormContacto.title", { framework: "React" })}
+            </p>
+          )}
+          {errors.telefono?.type === "minLength" && (
+            <p className="spartan-medium texto-error-formulario-contacto">
+              {t("MinCharFormContacto.title", { framework: "React" })}
+            </p>
+          )}
+          {errors.telefono?.type === "maxLength" && (
+            <p className="spartan-medium texto-error-formulario-contacto">
+              {t("MaxCharFormContacto.title", { framework: "React" })}
+            </p>
+          )}
+        </div>
+        <div className="contenedor-botones-formulario-contacto">
+          <button
+            className="spartan-medium boton-enviar-formulario-contacto boton-idioma transiciones estilos-interactivos-botones borde-redondeado"
+            type="submit"
+          >
+            {t("BtnEnviarFormContacto.title", { framework: "React" })}
+          </button>
+          <button
+            className="spartan-medium boton-restablecer-formulario-contacto boton-idioma transiciones estilos-interactivos-botones borde-redondeado"
+            type="button"
+            onClick={handleResetClick}
+          >
+            {t("BtnResetFormContacto.title", { framework: "React" })}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default FormularioContacto;
