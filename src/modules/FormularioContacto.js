9ff642e3b4df365ca0e2fe114fa793bbf40ba0e5
TRADEBOG <juan.miguel.jaramillo1984@gmail.com>
2023-06-29 13:17:00 -0500

    Otro intento con Zapier 3

--- src/modules/FormularioContacto.js
+++ src/modules/FormularioContacto.js
@@ -36,17 +36,23 @@ function FormularioContacto() {
         setForm({ ...form, [e.target.name]: e.target.value });
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = e => {
         e.preventDefault();
-        fetch('https://hooks.zapier.com/hooks/catch/15793138/3dimzst/', {
+        
+        const formData = new URLSearchParams();
+        for (const key in form) {
+            formData.append(key, form[key]);
+            }
+            
+            fetch('https://hooks.zapier.com/hooks/catch/15793138/3dimzst/', {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(form)
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: formData.toString()
             })
             .then(response => response.json())
             .then(data => console.log(data))
             .catch((error) => console.error('Error:', error));
-    };
+        };
 
     return (
         <div className="contenedor-formulario-contacto">
