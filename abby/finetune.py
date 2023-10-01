72687835deb96c5420265d25151b1632101aaa40
JuanJaramillo <juan.miguel.jaramillo1984@gmail.com>
2023-10-01 17:55:55 -0500

    llama-2-7b-chat-hf.py 8.0

--- abby/finetune.py
+++ abby/finetune.py
@@ -6,7 +6,7 @@ training = replicate.trainings.create(
         "train_data": "https://gist.githubusercontent.com/nateraw/055c55b000e4c37d43ce8eb142ccc0a2/raw/d13853512fc83e8c656a3e8b6e1270dd3c398e77/samsum.jsonl",
         "num_train_epochs": 1,
     },
-    destination="a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5"
+    destination="juanjaragavi/mind-mac-llama-2"
 )
 
 print(training)
