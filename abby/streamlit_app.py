424776a855789ec5a83604ad26d0193daaaef58f
JuanJaramillo <juan.miguel.jaramillo1984@gmail.com>
2023-10-01 06:59:11 -0500

    Testing Avoid Auto-Generate 3

--- abby/streamlit_app.py
+++ abby/streamlit_app.py
@@ -71,6 +71,9 @@ if st.session_state.messages[-1]["role"] != "assistant":
             response = generate_llama2_response(prompt)
             placeholder = st.empty()
             full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
             placeholder.markdown(full_response)
     message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
