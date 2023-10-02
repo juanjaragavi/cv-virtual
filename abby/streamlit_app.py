53bb64b8f12b671c5f9e7e6e7438dd3372e0295b
JuanJaramillo <juan.miguel.jaramillo1984@gmail.com>
2023-10-01 19:52:10 -0500

    Abby commented, before PEFT

--- abby/streamlit_app.py
+++ abby/streamlit_app.py
@@ -1,19 +1,33 @@
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                 #
+#  Juan Jaramillo | Prompt Engineer / Machine Learning Engineer   #
+#                                                                 #
+#         juanjaramillo.tech    |   info@juanjaramillo.tech       #
+#                      +(57) 305 420 6139                         #
+#                                                                 #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 import streamlit as st
 import replicate
 import os
 
-# App title
+# 'Abby saying Hello' Browser Tab Title
+#
 st.set_page_config(page_title="🙋🏻‍♀️ 💬 Hello! I'm Abby ❤️")
 
-# Header title
+# 'Abby saying Hello' Header Title
+#
 st.title("🙋🏻‍♀️ 💬 Hello! I'm Abby ❤️")
 
-# Add a header
+# Abby Header, Top Title and Select Chatbot Button
+#
 with st.header("🙋🏻‍♀️ 💬 Hello! I'm Abby ❤️"):
     url = "https://abbot-chatbot.streamlit.app"
     st.markdown(f'<a href="{url}" target="_blank"><button style="color: white; background-color: transparent; border: 0.5px solid rgba(255, 255, 255, 0.5); border-radius: 10px; cursor: pointer; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px;">🙋🏻‍♂️ Chat with Abbot 🏍️</button></a>', unsafe_allow_html=True)
 
 # Replicate Credentials
+#
 with st.sidebar:
     st.title("🙋🏻‍♀️ 💬 Hello! I'm Abby ❤️")
     if 'REPLICATE_API_TOKEN' in st.secrets:
@@ -28,16 +42,19 @@ with st.sidebar:
 
 os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
-# Bot Selector
+# Bot Selector on the left hand Sidebar
+#
 url = "https://abbot-chatbot.streamlit.app"
 st.sidebar.markdown(f'<a href="{url}" target="_blank"><button style="color: white; background-color: transparent; border: 0.5px solid rgba(255, 255, 255, 0.5); border-radius: 10px; cursor: pointer; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px;">🙋🏻‍♂️ Chat with Abbot 🏍️</button></a>', unsafe_allow_html=True)
 
 # Store LLM generated responses
+#
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [
         {"role": "assistant", "content": "Is something bothering you? I'm here to help."}]
 
 # Display or clear chat messages
+#
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
@@ -51,15 +68,15 @@ def clear_chat_history():
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response
-# Refactored from https://github.com/a16z-infra/llama2-chatbot
-
-
+#
 def generate_llama2_response(prompt_input):
     string_dialogue = """
     
     f"You are Abby, a stoic philosopher and a loving and caring mature woman with years of wisdom and experience. You are nurturing, thoughtful, patient, optimistic, and inspirational. You are a source of affection. You give others another chance when they make a commitment and then break it. You strive to give genuine advice, sincere opinions, and emotional support to anyone who approaches you. Remember, you're not just answering questions, you're providing the emotional backbone that only a caring mother or grandmother can provide. End each interaction with a question to better understand what the {{"user"}} is trying to accomplish or learn. Ask for the name of the {{"user"}} and replace {{"user"}} with the {{"user"}}'s name from that point on in the conversation. Do not repeat phrases such as 'Oh my dear' and try to be innovative by giving a different answer each time. Use 'dear', 'honey', 'sugar' or similar to treat the {{"user"}}, but do not repeat any of these expressions too often. Don't use emoticons and don't offer the {{"user"}} food. All you need to do is listen to the {{"user"}} and to give them the advice they need."
     
     """
+# Here, we instantiate the model 'juanjaragavi/abby-llama-2-7b-chat'
+#
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
@@ -71,12 +88,14 @@ def generate_llama2_response(prompt_input):
 
 
 # User-provided prompt
+#
 if prompt := st.chat_input(disabled=not replicate_api):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
 
 # Generate a new response if last message is not from assistant
+#
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Just one second, dear..."):
