753cf014efb558e7328709204d0e746dfdc6de01
juanjaragavi <info@juanjaramillo.tech>
2023-11-02 12:59:16 -0500

    Media Dir Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,15 @@ module.exports = {
         "league-spartan": ['"League Spartan"', "sans-serif"],
       },
       backgroundImage: {
-        dark: "url('https://media.juanjaramillo.tech/fondo-hero-dark-1440.webp')",
+        dark: "url('https://static.files.juanjaramillo.tech/fondo-hero-dark-1440.webp')",
         light:
-          "url('https://media.juanjaramillo.tech/fondo-hero-light-1440.webp')",
+          "url('https://static.files.juanjaramillo.tech/fondo-hero-light-1440.webp')",
         logodark:
-          "url('https://media.juanjaramillo.tech/logo-juan-jaramillo.webp')",
+          "url('https://static.files.juanjaramillo.tech/logo-juan-jaramillo.webp')",
         logolight:
-          "url('https://media.juanjaramillo.tech/logo-juan-jaramillo-light.webp')",
+          "url('https://static.files.juanjaramillo.tech/logo-juan-jaramillo-light.webp')",
         fondomovillight:
-          "url('https://media.juanjaramillo.tech/fondo-hero-light-1440.webp')",
+          "url('https://static.files.juanjaramillo.tech/fondo-hero-light-1440.webp')",
       },
     },
   },
